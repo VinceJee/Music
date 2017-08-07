7320ba84b6d112125d13b9e5c5acd598907b06f7
VinceJee <1067337034@qq.com>
2017-08-07 23:48:09 +0800

    initial demo code.

---
+++ Music/Music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Music
+//
+//  Created by zhumengjiao on 2017/8/7.
+//  Copyright © 2017年 VinceJee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Music/Music/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Music
+//
+//  Created by zhumengjiao on 2017/8/7.
+//  Copyright © 2017年 VinceJee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
